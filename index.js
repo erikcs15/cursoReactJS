f160601f76f300d052c19a9399b6df6df80525f2
Erik Corrales <jecs1594@gmail.com>
2019-02-21 16:35:30 -0700

    Usando ReactDOM y JSX

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//console.log("Hola mundo");
+
+//ReactDOM.render(que voy a renderizar, donde lo haré);
+const app = document.getElementById('app');
+const holaMundo=<h1>Hola mundo</h1>; 
+ReactDOM.render(holaMundo ,app);
+
+
+
